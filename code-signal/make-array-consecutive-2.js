b49af5b32dc95a1dfd0e9e522d88a4aa163f73ac
Kenny Park <kennyparc@gmail.com>
2021-12-16 04:51:39 +0000

    Completed make-array-consecutive-2.

---
+++ code-signal/make-array-consecutive-2.js
@@ -0,0 +1,20 @@
+function solution(statues) {
+  if (statues.length === 1) {
+    return 0;
+  }
+  let count = 0;
+  statues.sort(function (a, b) { return a - b });
+  for (let i = 1; i < statues.length; i++) {
+    if ((statues[i] - statues[i - 1]) !== 1) {
+      count += statues[i] - statues[i - 1]
+      count--
+    }
+  }
+  return count;
+}
+
+// alt solution
+
+// function solution(sequence) {
+//   return Math.max(...sequence) - Math.min(...sequence) + 1 - sequence.length
+// }
