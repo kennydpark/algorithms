744212a3d253659562bc3294f2ea6eb8b27bb5f4
Kenny Park <kennyparc@gmail.com>
2022-01-14 06:11:55 +0000

    Completed 'convenience store'.

---
+++ edabit/convenience-store.js
@@ -0,0 +1,30 @@
+const changeEnough = (change, amountDue) => {
+  let quarters = change[0] *= 0.25;
+  let dimes = change[1] *= 0.1;
+  let nickles = change[2] *= 0.05;
+  let pennies = change[3] *= 0.01;
+
+  let total = quarters + dimes + nickles + pennies
+
+  if (total >= amountDue) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// alt
+
+const changeEnough2 = (change, amountDue) => {
+  let sum = change[0] * 0.25 + change[1] * 0.1 + change[2] * 0.05 + change[3] * 0.01;
+  return sum >= amountDue;
+}
+
+// .reduce method
+
+const sum = arr => arr.reduce((total, num) => total + num, 0);
+
+const values = [0.25, 0.1, 0.05, 0.01];
+
+const changeEnough = (change, amountDue) =>
+  sum(change.map((amount, i) => amount * values[i])) >= amountDue;
